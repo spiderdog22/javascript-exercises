369d8a90fb7332000364f2b419a3eedee1b16178
spiderdog22 <na>
2024-09-17 18:47:48 -0500

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, repeats) {
+    if (repeats < 0)
+        return 'ERROR'
 
+    let result = '';
+
+    for (let i = 0; i < repeats; i++)
+        result += str;
+
+    return result;
 };
 
 // Do not edit below this line
