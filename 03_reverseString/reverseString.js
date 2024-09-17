653cef44d8e045c09f2fb5817f5ce8854e07d8f2
spiderdog22 <na>
2024-09-17 18:57:31 -0500

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitedStr = str.split('');
+    splitedStr.reverse();
 
+    return splitedStr.join('');
 };
 
 // Do not edit below this line
