2b22fa13f49a0ed812b96e5a63ee5f6497a0db52
spiderdog22 <na>
2024-09-17 20:09:05 -0500

    Solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleByFour = year % 4 == 0;
+    let isCentury = year % 100 == 0;
+    let divisiblebyFourHundred = year % 400 == 0;
 
+    return (divisibleByFour && !isCentury) || divisiblebyFourHundred;
 };
 
 // Do not edit below this line
