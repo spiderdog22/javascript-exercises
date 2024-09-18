3acd2fd46a7c15e7db7ee42ffae2681fb56a57e2
spiderdog22 <na>
2024-09-17 20:17:09 -0500

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const roundNumber = function(number, decimals) {
+  return Math.round(number * 10 * decimals) / (10 * decimals);
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversionResult = (fahrenheit - 32) * (5/9);
+  return roundNumber(conversionResult, 1);
+};
+
+const convertToFahrenheit = function(celcius) {
+  let conversionResult = celcius * (9/5) + 32;
+  return roundNumber(conversionResult, 1);
 };
 
 // Do not edit below this line
