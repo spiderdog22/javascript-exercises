666888ce947150c9df1f52d0d72339df63791b21
spiderdog22 <na>
2024-09-17 19:57:55 -0500

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (( a < 0 || b < 0 ) ||
+        // 'Math.floor(a) !== a': Checks if floor'ed 'a' is the same as 'a'.
+        // It also serves for check the correct type. If 'a' is not a number,
+        // then it will return 'ERROR'
+        ( Math.floor(a) !== a || Math.floor(b) !== b)) {
+        return 'ERROR';
+    }
 
+    let minor = b > a ? a : b;
+    let major = b > a ? b : a;
+
+    // Substracts one summation (N*(N+1)/2), that represents all the first N natural numbers
+    // added all together; with another.
+    return (major * (major + 1))/2 - (minor * (minor - 1))/2
 };
 
 // Do not edit below this line
