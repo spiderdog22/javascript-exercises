a4181ade6675bb683cc13bcc7f510330ae7d7265
spiderdog22 <na>
2024-09-17 19:30:57 -0500

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
+
+    array.forEach((el) => {
+
+        /*
+        My solution
+        let descarted = false;
+
+        for (let arg of args)
+            if (arg === el)
+                descarted = true;
+        
+        if (!descarted)
+            result.push(el);
+        */
+
+        if (!args.includes(el)) {
+            result.push(el);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
