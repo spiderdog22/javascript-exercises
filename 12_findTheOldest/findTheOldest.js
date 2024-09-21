066bc59f78000d597e7ec9ed6d9944b2ca745042
spiderdog22 <na>
2024-09-21 18:41:27 -0500

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = arr.reduce( (acc, el) => {
+        let ageAcc = (acc.yearOfDeath ? acc.yearOfDeath : new Date().getFullYear()) - acc.yearOfBirth;
+        let ageEl = (el.yearOfDeath ? el.yearOfDeath : new Date().getFullYear()) - el.yearOfBirth;
+        return ageAcc < ageEl ? el : acc;
+    });
 
+    return oldest;
 };
 
 // Do not edit below this line
