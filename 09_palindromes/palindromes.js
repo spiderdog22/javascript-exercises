1886300e98f26c97aeedf1bf83d939c97e2cd77f
spiderdog22 <na>
2024-09-21 17:50:37 -0500

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
 };
+
+const palindromes = function (str) {
+    let letters = str.replaceAll(/[^a-zA-B0-9]+/gi, '').toLowerCase();
+    console.log(letters);
+    let reversed = reverseString(letters);
+    console.log(reversed);
+    console.log(letters === reversed);
+    return letters === reversed;
+};
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
