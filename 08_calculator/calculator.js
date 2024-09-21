3c23b126bc94d26da6e457d5b77ffcc39707682c
spiderdog22 <na>
2024-09-21 16:29:41 -0500

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce( (acc, el) => acc + el , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (acc, el) => acc * el , 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let res = 1;
+
+  while (power-- != 0)
+    res *= number;
+
+    return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1;
+  return Array.from(Array(number + 1).keys())
+    .slice(1)
+    .reduce( (acc, el) => acc * el, 1);
 };
 
 // Do not edit below this line
